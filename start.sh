3b02c0542732bfe57574a055f92dd104b29d3c5e
oleewere <oleewere@gmail.com>
2015-05-08 17:51:43 +0200

    CLOUD-672 fix crontab

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$CRON_EXPR" ]; then
-  CRON_EXPR="6 0	* * *"
+  CRON_EXPR="0 6	* * *"
   echo "CRON_EXPR environment variable is not set. Set to default: $CRON_EXPR"
 else
   echo "CRON_EXPR environment variable set to $CRON_EXPR"
